41fd34df65522fdb66365b386b96d4aff5059f5a
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2022-02-24 18:17:35 -0700

    🏗️ Changing the management to prepare for routes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,14 @@
-import Axios from 'axios';
-import { useEffect, useState } from 'react';
-import Monster from './components/Monster';
+import Home from './routes/Home';
 
 function App() {
-  
-  const [isLoading, setIsLoading] = useState(true);
-  const [monsters, setMonsters] = useState([]);
-  const [searchWord, setSearchWord] = useState("");
-
-  useEffect(() => {
-    loadData();
-  }, [])
-  
-  const loadData = () => {
-    Axios.get("https://botw-compendium.herokuapp.com/api/v2/category/monsters").then((response) => {
-      setMonsters(response.data.data);
-      setIsLoading(false);
-    })
-  }
-
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.includes(searchWord.toLowerCase());
-  })
-
-  if(isLoading){
-    return (
-      <div className='App'>
-        <header>
-          <h1>Hyrule's Monsters</h1>
-          <h3>Now loading...</h3>
-        </header>
-      </div>
-    );
-  }
-
   return (
     <div className="App">
       <header>
         <h1>Hyrule's Monsters</h1>
-        <input 
-          type='text' 
-          placeholder='Type a monster' 
-          onChange={(e) => {
-            setSearchWord(e.target.value);
-          }}
-        />
       </header>
-      <section>
-        {filteredMonsters.map((monster) => {
-          return (
-            <Monster 
-              name={monster.name} 
-              image={monster.image} 
-              description={monster.description}
-              key={monster.id}
-            />
-          );
-        })}
-      </section>
+      <main>
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/routes/Home.js
@@ -0,0 +1,63 @@
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+import Monster from './../components/Monster';
+
+function Home() {
+  
+  const [isLoading, setIsLoading] = useState(true);
+  const [monsters, setMonsters] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
+  useEffect(() => {
+    loadData();
+  }, [])
+  
+  const loadData = () => {
+    Axios.get("https://botw-compendium.herokuapp.com/api/v2/category/monsters").then((response) => {
+      setMonsters(response.data.data);
+      setIsLoading(false);
+    })
+  }
+
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.includes(searchWord.toLowerCase());
+  })
+
+  filteredMonsters.sort((a, b) => {
+    return a.id - b.id;
+  })
+
+  if(isLoading){
+    return (
+      <div className='Home'>
+        <h3>Now loading...</h3>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Home">
+      <input 
+        type='text' 
+        placeholder='Type a monster' 
+        onChange={(e) => {
+          setSearchWord(e.target.value);
+        }}
+      />
+      <section>
+        {filteredMonsters.map((monster) => {
+          return (
+            <Monster 
+              name={monster.name} 
+              image={monster.image} 
+              description={monster.description}
+              key={monster.id}
+            />
+          );
+        })}
+      </section>
+    </div>
+  );
+}
+
+export default Home;
